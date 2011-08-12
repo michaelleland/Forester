a9f9163ad581c12eb9239d481d4d293a3815a949
Teemu Vainio <tim@silvertreksystems.com>
2011-08-12 16:57:54 -0700

    Yay, work started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forester::Application.config.secret_token = '20165d90b6995180450574b3b233e6680f8a65ca5d3efd32728b56f95fcba89cd29ab223a6edb8dfe85f9a477f0f9a136579ad17bd3caef70cafb5524c895e57'
