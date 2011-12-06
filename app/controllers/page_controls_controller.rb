c508cb7a18f58b41e18b586531327a49fe1141d5
Teemu Vainio <tim@silvertreksystems.com>
2011-12-05 18:03:08 -0800

    COmmented code and deleted old code

--- app/controllers/page_controls_controller.rb
+++ app/controllers/page_controls_controller.rb
@@ -100,6 +100,7 @@ class PageControlsController < ApplicationController
     render "get_tickets.html.erb"
   end
   
+  #Ajax action
   #Gets receipts according to given owner type and owner id
   def get_receipts
     @job = Job.find(params[:id])
@@ -118,6 +119,9 @@ class PageControlsController < ApplicationController
     end
   end
   
+  #Ajax action
+  #This action was used in ticket entry to open and close mbf and tonnage fields.
+  #This feature is note in use anymore.
   def get_load_type
     @job = Job.find_by_name(params[:job_name])
     @destination = Destination.find_by_name(params[:destination_name])
@@ -171,60 +175,4 @@ class PageControlsController < ApplicationController
     render :text => @answer
     
   end
-  
-  def add_specie
-    
-  end
-  
-  def delete_specie
-    
-  end
-  
-  def import_jobs_of_partner
-    if params[:id] != "0"
-      @l_asg = LoggerAssignment.find_all_by_partner_id(params[:id])
-      
-      @l_job_ids = @l_asg.collect {|i| i.job_id}.flatten
-      
-      @logger_jobs = Job.find(@l_job_ids)
-      
-      @t_asg = TruckerAssignment.find_all_by_partner_id(params[:id])
-      
-      @t_job_ids = @t_asg.collect {|i| i.job_id}.flatten
-      
-      @trucker_jobs = Job.find(@t_job_ids)
-      
-      @jobs = @trucker_jobs + @logger_jobs
-      @jobs.flatten!
-      @jobs.uniq!
-      @jobs = @jobs.sort_by {|i| i.name } 
-    end
-  end
-  
-  def import_jobs_of_logger
-    if params[:id] != "0"
-      @l_asg = LoggerAssignment.find_all_by_partner_id(params[:id])
-      
-      @l_job_ids = @l_asg.collect {|i| i.job_id}.flatten
-      
-      @logger_jobs = Job.find(@l_job_ids)
-    end
-  end
-  
-  def import_jobs_of_trucker
-    if params[:id] != "0"
-      @t_asg = TruckerAssignment.find_all_by_partner_id(params[:id])
-      
-      @t_job_ids = @t_asg.collect {|i| i.job_id}.flatten
-      
-      @trucker_jobs = Job.find(@t_job_ids)
-    end
-  end
-  
-  def import_jobs_of_owner
-    if params[:id] != "0"
-      @owner_jobs = Job.find_all_by_owner_id(params[:id])
-    end
-  end
-  
 end
